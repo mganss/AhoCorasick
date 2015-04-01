fc2277bc580e53214c497210790067b3aa3137c9
Michael Ganss <michael.ganss@updatestar.com>
2015-04-01 12:23:43 +0200

    Add custom char comparison to extension methods

--- AhoCorasick/Extensions.cs
+++ AhoCorasick/Extensions.cs
@@ -28,5 +28,29 @@ namespace Ganss.Text
         {
             return new AhoCorasick(words).Search(text);
         }
+
+        /// <summary>
+        /// Determines whether this instance contains the specified words.
+        /// </summary>
+        /// <param name="text">The text.</param>
+        /// <param name="comparer">The comparer used to compare individual characters.</param>
+        /// <param name="words">The words.</param>
+        /// <returns>The matched words.</returns>
+        public static IEnumerable<WordMatch> Contains(this string text, IEqualityComparer<char> comparer, IEnumerable<string> words)
+        {
+            return new AhoCorasick(comparer, words).Search(text);
+        }
+
+        /// <summary>
+        /// Determines whether this instance contains the specified words.
+        /// </summary>
+        /// <param name="text">The text.</param>
+        /// <param name="comparer">The comparer used to compare individual characters.</param>
+        /// <param name="words">The words.</param>
+        /// <returns>The matched words.</returns>
+        public static IEnumerable<WordMatch> Contains(this string text, IEqualityComparer<char> comparer, params string[] words)
+        {
+            return new AhoCorasick(comparer, words).Search(text);
+        }
     }
 }
